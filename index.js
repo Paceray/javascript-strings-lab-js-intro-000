e490b6105d9313bc549b53b877dd3faaa5e12ece
raymond weiland <weilandr1971@gmail.com>
2019-12-30 00:39:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = 'Hello, Neil deGrasse Tyson'
 
+var greetSpecialGuest = 'specialGuest'
+
 var topic = "space";
 
 var conversation = `${topic}`;
